8f5df13819954bf209f4454eafcbe4d2260eba2f
Libor Duda <xdudal@stuba.sk>
2021-10-10 19:23:12 +0200

    add voting process and footer section, add configuration file

---
+++ config.php
@@ -0,0 +1,64 @@
+<?php
+return [
+    'site_title' => 'E-voľby | TP@FIIT STU',
+    'site_name' => 'E-voľby',
+    'email' => 'tim172021@googlegroups.com',
+
+    'advantages' => [
+        [
+            'name' => 'Výhoda',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ],
+        [
+            'name' => 'Výhoda 2',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ],
+        [
+            'name' => 'Výhoda 3',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ],
+        [
+            'name' => 'Výhoda 4',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ]
+    ],
+
+    'voting_items' => [
+        [
+            'name' => 'Step 1',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ],
+        [
+            'name' => 'Step 2',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ],
+        [
+            'name' => 'Step 3',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ],
+    ],
+
+    'team_members' => [
+        [
+            'name' => 'For Biden',
+            'img_path' => 'img/biden.jpg',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ],
+        [
+            'name' => 'For Biden',
+            'img_path' => 'img/biden.jpg',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ],
+        [
+            'name' => 'For Biden',
+            'img_path' => 'img/biden.jpg',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ],
+        [
+            'name' => 'For Biden',
+            'img_path' => 'img/biden.jpg',
+            'description' => 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+        ],
+    ],
+
+];
--- index.php
+++ index.php
@@ -1,8 +1,9 @@
+<?php $config = require_once 'config.php'; ?>
 <html>
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="utf-8">
-        <title>E-voľby | TP FIIT</title>
+        <title><?php echo $config['site_title']; ?></title>
         <link rel="stylesheet" href="dist/app.css">
         <script src="dist/app.js"></script>
     </head>
@@ -10,7 +11,7 @@
     <header>
         <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light">
             <div class="container-lg">
-                <a class="navbar-brand" href="#">E-voľby</a>
+                <a class="navbar-brand" href="#"><img src="img/logo.svg" alt="team logo" class="team-logo"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
@@ -18,6 +19,9 @@
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="#advantages">Výhody</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#voting-proces">Ako votiť</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="#team">Tím</a>
@@ -34,9 +38,14 @@
         </nav>
     </header>
     <main>
-        <section id="hero" class="py-5" style="background-image: url(img/hero.jpg)">
-            <div class="hero-content container-lg">
+        <section id="hero" class="has-background-image has-overlay text-white py-5" style="background-image: url(img/hero.jpg)">
+            <div class="inner-content container-lg">
                 <h1 class="text-center">Prvé elektronické voľby na Slovensku</h1>
+                <div class="mt-5">
+                    <div class="text-center">
+                        <a href="#voting-proces" class="btn btn-secondary btn-lg">Ako fungujú naše voľby</a>
+                    </div>
+                </div>
             </div>
             <div class="overlay"></div>
         </section>
@@ -45,36 +54,66 @@
             <div class="container-lg">
                 <h2 class="text-center mb-4">Výhody nášho riešenia</h2>
                 <div class="row g-5">
-                    <?php for ($i = 0; $i < 4; $i++): ?>
+                    <?php foreach ($config['advantages'] as $advantage): ?>
                         <div class="col-md-6">
                             <div class="advantage p-5">
-                                <h4 class="advantage-name">Výhoda</h4>
+                                <h4 class="advantage-name"><?php echo $advantage['name'] ?></h4>
                                 <div class="advantage-description">
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been therieially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                    <?php echo $advantage['description'] ?>
                                 </div>
                             </div>
                         </div>
-                    <?php endfor; ?>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </section>
+        <section id="voting-proces" class="has-background-image has-overlay py-5" style="background-image: url(img/parliament.png)">
+            <div class="container-lg inner-content">
+                <h2 class="text-center text-white mb-4">Ako voliť</h2>
+                <div class="row">
+                    <div class="col-md-6 mb-0 mb-md-0">
+                        <div class="accordion accordion-flush" id="voting-accordion">
+                            <?php $items_count = count($config['voting_items']); ?>
+                            <?php foreach ($config['voting_items'] as $key => $item): ?>
+                                <div class="accordion-item <?php if($key < $items_count - 1) echo 'mb-3'; ?>">
+                                    <h2 class="accordion-header" id="flush-headingOne">
+                                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#item-<?php echo $key; ?>" aria-expanded="false" aria-controls="item-<?php echo $key; ?>">
+                                            <?php echo $item['name']; ?>
+                                        </button>
+                                    </h2>
+                                    <div id="item-<?php echo $key; ?>" class="accordion-collapse collapse" aria-labelledby="item-<?php echo $key; ?>" data-bs-parent="#voting-accordion">
+                                        <div class="accordion-body"><?php echo $item['description']; ?></div>
+                                    </div>
+                                </div>
+                            <?php endforeach; ?>
+                        </div>
+                    </div>
+                    <div class="col-md-6"></div>
+                </div>
+            </div>
+            <div class="overlay"></div>
+        </section>
         <section id="team" class="py-5">
-            <div class="container-lg">
+            <div class="container-lg overflow-hidden">
                 <h2 class="text-center mb-4">Tím</h2>
                 <div class="row g-4">
-                    <?php for ($i = 0; $i < 8; $i++): ?>
+                    <?php foreach ($config['team_members'] as $member): ?>
                     <div class="col-md-6 col-lg-3">
                         <div class="member">
                             <div class="member-image">
                                 <div class="image-wrapper fixed-image-ratio-1-1 rounded mb-3">
-                                    <img src="img/biden.jpg" alt="member image">
+                                    <?php if(!empty($member['img_path'])): ?>
+                                    <img src="<?php echo $member['img_path']; ?>" alt="member image">
+                                    <?php endif; ?>
                                 </div>
                             </div>
-                            <h4 class="member-name">Joe Biden</h4>
-                            <div class="member-description">Lorem ipsum dolor sit amet, consus molestiae numquam quaerat quam repellendus saepe! Autem distinctio eos magnam molestias praesentium quidem tempora voluptatem voluptates!</div>
+                            <h4 class="member-name"><?php echo $member['name']; ?></h4>
+                            <div class="member-description">
+                                <?php echo $member['description']; ?>
+                            </div>
                         </div>
                     </div>
-                    <?php endfor; ?>
+                    <?php endforeach; ?>
                 </div>
             </div>
         </section>
@@ -86,21 +125,25 @@
                 </div>
             </div>
         </section>
-        <section id="contact" class="py-5">
-            <div class="container-lg">
-                <h2 class="text-center mb-4">Kontakt</h2>
-                <div class="row">
-                    <div class="col-md-8 mx-auto">
-                        <h4 class="text-center"><a href="mailto:tim172021@googlegroups.com">tim172021@googlegroups.com</a></h4>
+    </main>
+    <footer id="contact" class="footer has-background-image has-overlay text-white py-5" style="background-image: url(img/footer.png)">
+        <div class="container-lg inner-content">
+            <div class="row">
+                <div class="col-md-6 mb-5 mb-md-0">
+                    <img src="img/logo_white.svg" alt="team logo" class="team-logo mb-4">
+                    <h2 class="mb-4">Prvé voľby na Slovensku</h2>
+                    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam aperiam atque eius eveniet iste modi neque nihil nobis, nostrum quas quibusdam quis quisquam recusandae repellendus repudiandae rerum sit ullam voluptates.</div>
+                </div>
+                <div class="col-md-6">
+                    <h2 class="mb-4">Tím č.17</h2>
+                    <a href="mailto:<?php echo $config['email']; ?>" class="text-reset"><?php echo $config['email']; ?></a>
+                    <div class="mt-5">
+                        <img src="img/fiit_logo.png" alt="fiit logo">
                     </div>
                 </div>
             </div>
-        </section>
-    </main>
-    <footer class="bg-dark text-white py-3">
-        <div class="text-center">
-            &copy; 2021 E-voľby
         </div>
+        <div class="overlay"></div>
     </footer>
     </body>
 </html>
